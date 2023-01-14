238052df7a97ca2deeeeeb7b695ae76206a262c9
luminousbamba <122699602+luminousbamba@users.noreply.github.com>
2023-01-14 22:36:21 +0000

    Add files via upload

---
+++ Untitled-1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Document sans titre</title>
+</head>
+
+<body>
+<form id="form2" name="form2" method="post" action="">
+  ff
+    <input type="submit" name="Submit" value="Envoyer" />
+</form>
+<form id="form1" name="form1" method="post" action="">
+</form>
+</body>
+</html>
